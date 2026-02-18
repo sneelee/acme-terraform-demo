d29c19a60227e45063089d1bf77942c75990fb5d
sneelee <134574176+sneelee@users.noreply.github.com>
2026-02-18 12:07:33 -0500

    Add outputs for S3 bucket details in outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,20 @@
+output "bucket_name" {
+  description = "Name of the S3 bucket created"
+  value       = aws_s3_bucket.acme_assets.bucket
+}
+
+output "bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.acme_assets.arn
+}
+
+output "bucket_region" {
+  description = "AWS region where the bucket was created"
+  value       = var.aws_region
+}
+
+output "environment" {
+  description = "Deployment environment"
+  value       = var.environment
+}
+```
